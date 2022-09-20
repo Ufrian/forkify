ba01ae077031a15ea7cda0e017cc483f0330debb
Ufrian <ufrian.contato@gmail.com>
2022-09-20 07:46:44 -0300

    Removed welcome

--- src/js/controller.js
+++ src/js/controller.js
@@ -130,10 +130,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the Application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -143,7 +139,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 
 init();
